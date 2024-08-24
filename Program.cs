3d1451eb5559bc5d1ad11e64fcc01bb831954288
jctejadilla <jctejadilla2004@gmail.com>
2024-08-24 19:56:30 +0800

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,29 +1,22 @@
 ﻿using System;
-namespace ComputeAverageApp{
+namespace DataTypesApp{
 
-public class ComputeAverageProgram
+public class DataTypesProgram
 {
     public static void Main(string[] args)
     {
+        Console.WriteLine ("Enter a pieces of apple: ");
+        int a = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine ($"Enter total price of {a} apple(s): ");
+        double b = Convert.ToDouble(Console.ReadLine());
         
-        double[] grades = new double[5];
-
-        for (int i = 0; i < grades.Length; i++)
-        {
-            Console.Write($"Enter grade {i + 1}: ");
-            grades[i] = double.Parse(Console.ReadLine());
-        }
+        Console.WriteLine ("=====================================");
+        Console.WriteLine ($"the total price of {a} apple(s) is " + b);
+        Console.WriteLine ("the value of original price is " + b);
         
-        double sum = 0;
-        for (int i = 0; i < grades.Length; i++)
-        {
-            sum += grades[i];
-        }
-        double average = sum / grades.Length;
-        double roundedAverage = Math.Round(average);
-        
-        Console.WriteLine($"The average of the grades is: {average:F2}");
-        Console.WriteLine($"The rounded average is: {roundedAverage}");
+        int c = (int) b;
+        Console.WriteLine ("the value of converted price is " + c);
+        Console.WriteLine ("=====================================");
     }
 }
 }
