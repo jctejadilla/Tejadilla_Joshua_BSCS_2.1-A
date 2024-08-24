f77ed089b1d24426b9ccf78fc05dca2eeec73d86
jctejadilla <jctejadilla2004@gmail.com>
2024-08-24 19:53:38 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace ComputeAverageApp{
+
+public class ComputeAverageProgram
+{
+    public static void Main(string[] args)
+    {
+        
+        double[] grades = new double[5];
+
+        for (int i = 0; i < grades.Length; i++)
+        {
+            Console.Write($"Enter grade {i + 1}: ");
+            grades[i] = double.Parse(Console.ReadLine());
+        }
+        
+        double sum = 0;
+        for (int i = 0; i < grades.Length; i++)
+        {
+            sum += grades[i];
+        }
+        double average = sum / grades.Length;
+        double roundedAverage = Math.Round(average);
+        
+        Console.WriteLine($"The average of the grades is: {average:F2}");
+        Console.WriteLine($"The rounded average is: {roundedAverage}");
+    }
+}
+}
